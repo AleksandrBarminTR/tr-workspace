6fd445bc65467ca1b98571ab062fd7d3baa1c431
Aleksandr Barmin <56252277+AleksandrBarminTR@users.noreply.github.com>
2020-12-18 19:01:40 +0300

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,21 +1,8 @@
-name 'test_book'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'All Rights Reserved'
-description 'Installs/Configures test_book'
+name 'tr-workspace'
+maintainer 'Aleksandr Barmin'
+maintainer_email 'Aleksandr.Barmin@thomsonreuters.com'
+description 'Installs/Configures tr-workspace'
 version '0.1.0'
 chef_version '>= 15.0'
 
 depends 'git'
-
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/test_book/issues'
-
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/test_book'
