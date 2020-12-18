eb04ef33441e3a4315760239def60dd03503d493
Aleksandr Barmin <56252277+AleksandrBarminTR@users.noreply.github.com>
2020-12-18 19:01:04 +0300

    Update Policyfile.rb

--- Policyfile.rb
+++ Policyfile.rb
@@ -1,24 +1,18 @@
-# Policyfile.rb - Describe how you want Chef Infra Client to build your system.
-#
-# For more information on the Policyfile feature, visit
-# https://docs.chef.io/policyfile/
-
-# A name that describes what the system you're building with Chef does.
-name 'test_book'
+name 'tr-workspace'
 
 # Where to find external cookbooks:
 default_source :supermarket
 
 # run_list: chef-client will run these recipes in the order specified.
-run_list 'test_book::default', 
-        'test_book::corretto_8',
-        'test_book::corretto_11',
-        'test_book::apache_maven',
-        'test_book::gradle',
-        'test_book::vscode',
-        'test_book::intellij_idea'
+run_list 'tr-workspace::default', 
+        'tr-workspace::corretto_8',
+        'tr-workspace::corretto_11',
+        'tr-workspace::apache_maven',
+        'tr-workspace::gradle',
+        'tr-workspace::vscode',
+        'tr-workspace::intellij_idea'
 
 # Specify a custom source for a single cookbook:
-cookbook 'test_book', path: '.'
+cookbook 'tr-workspace', path: '.'
 
 cookbook 'git', '~> 10.1.0', :supermarket
